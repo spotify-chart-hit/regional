25a3f6ef363f6396a1761b6ba2e90466316d2f9e
wonytest0-dev <wonytest0@gmail.com>
2026-05-14 18:26:29 +0700

    Update auto-token.js

--- auto-token.js
+++ auto-token.js
@@ -1,1 +1,87 @@
+const { chromium } =
+require("playwright");
+
+async function getToken() {
+
+const browser =
+await chromium.launch({
+
+headless: true
+
+});
+
+const context =
+await browser.newContext({
+
+storageState:
+"auth.json"
+
+});
+
+const page =
+await context.newPage();
+
+let token = null;
+
+page.on(
+
+"request",
+
+request => {
+
+const url =
+request.url();
+
+const auth =
+request.headers()
+.authorization;
+
+if (
+
+url.includes(
+"charts-spotify-com-service.spotify.com/auth/v0/charts"
+)
+
+&&
+
+auth?.startsWith(
+"Bearer "
+)
+
+) {
+
+token = auth;
+
+}
+
+}
+
+);
+
+await page.goto(
+
+"https://charts.spotify.com/charts/view/regional-global-weekly/latest"
+
+);
+
+await page.waitForTimeout(
+5000
+);
+
+await browser.close();
+
+if (!token) {
+
+throw new Error(
+"NO TOKEN FOUND 😭"
+);
+
+}
+
+return token;
+
+}
+
+module.exports =
+getToken;
 
